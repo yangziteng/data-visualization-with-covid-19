a15a12172fad74b6ced9705919f97563a80b2e90
yangziteng <70270955+yangziteng@users.noreply.github.com>
2023-04-17 13:29:00 +0800

    修改样式

--- .history/data-visualization-with-covid-19/vue.config_20230122140749.js
+++ .history/data-visualization-with-covid-19/vue.config_20230122140749.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  publicPath: isProduction ? './' : './',//为项目中的所有资源(js、css、img)指定一个基础路径
+  publicPath: isProduction ? './' : '/',//为项目中的所有资源(js、css、img)指定一个基础路径
   devServer: {
     proxy: {
       '/': {//根据请求路径，匹配所有以/a开头的路径
@@ -9,7 +9,7 @@ module.exports = {
         secure: false,  // 如果是https开头，要设置为true
         changeOrigin: true,  //为true时，发送请求头中的host会设置成target。为false，则不变。默认为true
         // pathRewrite: { '/api': '' },// 发送请求时，请求路径重写：将 /a/xxx --> /xxx （去掉/a）
-        
+
       }
     },
     // port: '8080'//可自己修改端口
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ data-visualization-with-covid-19
 - cd data-visualization-with-covid-19
 - npm install
 - npm run start
+# npm和node版本
+node v16.14.0
+npm 9.1.3
+# 效果展示
+[![p9CcKL6.png](https://s1.ax1x.com/2023/04/17/p9CcKL6.png)](https://imgse.com/i/p9CcKL6)
+[![p9Cc8Fe.png](https://s1.ax1x.com/2023/04/17/p9Cc8Fe.png)](https://imgse.com/i/p9Cc8Fe)
